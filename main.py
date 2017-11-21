ff444225e4b1e35af26b5856e83702e56a829acd
ravenred <ravenred@users.noreply.github.com>
2017-11-21 11:18:04 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,96 @@
+"""
+Main.py All functions & Libraries to run go here
+"""
+import os   #imports OS features
+from tld import get_tld     #Extracts the top level domains from URL
+
+
+def main():
+    banner()
+
+
+"""
+Banner Function
+"""
+
+def banner():
+    print("******************************\n"
+          "              ^               \n"                                         
+          "             | |              \n"                                        
+          "             |V|              \n"                                                 
+          "             |A|              \n"                                        
+          "           _||L||_            \n"                                              
+          "           |  I  |            \n"                                            
+          "           \  A  /            \n"                                                          
+          "           /  N  \            \n"                                             
+          "          /|  T  |\           \n"                            
+          "         /    |    \          \n"                                               
+          "        /     |     \         \n"                                               
+          "      /|      |      |\       \n"                                         
+          "     /|       |       |\      \n"                                               
+          "    / |       !       | \     \n"                                               
+          "   / @|       !       |@ \    \n"                                               
+          "  |....  ___  !  ___  ....|   \n"                                              
+          "  |__----   \_!_/   ----__|   \n"                                          
+          "              V               \n"
+          "       Website Scanner        \n"
+          "******************************")
+
+
+"""
+Makes a home directory function
+"""
+
+
+def make_home_directory(directory):
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+"""
+Write file function
+"""
+
+
+def write_to_file(path, output):
+    f = open(path, 'w')
+    f.write(output)
+
+"""
+Gets the domain name from URL
+"""
+
+
+def get_domain_name(url):
+    domain_name = get_tld(url)
+    return domain_name
+
+"""
+Get website ip address
+"""
+
+
+def get_ip(url):
+    command = "host "+url
+    execute = os.popen(command)
+    result = str(execute.read())
+    finder = result.find('has address')+12
+    ip = result[finder:].splitlines()[0]
+    return ip
+
+"""
+NMAP Scan Function
+@:param scan Scan Options
+@:param ip IP Address
+"""
+
+
+def get_nmap(scan, ip):
+    command = "nmap "+scan+" "+ip
+    execute = os.popen(command)
+    nmap = str(execute.read())
+    return nmap
+
+
+if __name__ == '__main__':
+    main()
+
