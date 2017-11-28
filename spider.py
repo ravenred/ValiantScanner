ce46dc887f6239e6bbce60f2a17a0738273856ea
root <ianoconnell01@gmail.com>
2017-11-28 15:13:51 +0000

    spider.py Not working but no errors

--- spider.py
+++ spider.py
@@ -1,3 +1,20 @@
 """
 spider.py used to gather all internal links
 """
+import urllib
+import re
+
+
+def get_links(url):
+
+    path = "https://"+url
+    requests = urllib.urlopen(path, data=None)
+    links = requests.read()
+    print(links)
+
+    find_links = re.findall('<a href=(.*?)>', str(links))
+
+    for a in find_links:
+        print(a)
+
+    get_links("www.itb.ie")
