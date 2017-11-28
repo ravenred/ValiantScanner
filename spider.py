7d5e916f6024f3b053f69b1eb951c548a1d82df4
root <ianoconnell01@gmail.com>
2017-11-28 22:25:28 +0000

    spider.py Working gets all links just not formatted

--- spider.py
+++ spider.py
@@ -10,11 +10,13 @@ def get_links(url):
     path = "https://"+url
     requests = urllib.urlopen(path, data=None)
     links = requests.read()
-    print(links)
+    # print(requests.status)
 
-    find_links = re.findall('<a href=(.*?)>', str(links))
+    find_links = re.findall('<a href="(.*?)">', str(links))     # Uses re library to find all href sources
 
     for a in find_links:
-        print(a)
 
-    get_links("www.itb.ie")
+            print(a.split(" ")[0])  # Deletes all characters after whitespace
+
+
+get_links("www.itb.ie")
